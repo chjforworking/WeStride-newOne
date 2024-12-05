8b9e130f247978967df127b863051c9663f2ff43
PC <chjforworking@gmail.com>
2024-12-05 13:43:59 +0700

    update 05/12

--- 03 JS Basic/87 ถอดรหัส.js
+++ 03 JS Basic/87 ถอดรหัส.js
@@ -1,1 +1,62 @@
 //ถอดรหัส ตำแหน่งห่าง 5 ลำดับ
+/*
+
+*/
+let arr = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+let arrAns = [];
+
+function decrypt(str) {
+  //str = xvo+5
+  let splitStr = str.split("");
+  for (let x of splitStr) {
+    if (x != " ") {
+      let inputIndex = arr.indexOf(x); //1,2,3,4...
+      let nextIndex = inputIndex + 5;
+      let nextValue = arr[nextIndex];
+      if (nextValue) {
+        arrAns.push(nextValue);
+      } else {
+        let remainLoop = arr.length - 1 - inputIndex;
+        let newLoop = 5 - remainLoop;
+        let ans = arr[newLoop - 1];
+        arrAns.push(ans);
+      }
+    } else {
+      arrAns.push(x);
+    }
+  }
+  let answer = arrAns.join("");
+  return answer;
+}
+
+//console.log(decrypt("XVO"));
+//console.log("/////////////");
+console.log(decrypt("HT IVHZ DN W"));
---
+++ 03 JS Basic/88 เข้ารหัส.js
@@ -0,0 +1,18 @@
+//function เข้ารหัส A -> N , B -> O , C -> P
+
+let aToZ = ["a","b","c",'d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+function decode(str){
+    let splitStr = str.split('');
+    for (const x of splitStr) {
+        let inputIndex = aToZ.indexOf(x) ;//1,2,3
+        let nextIndex = inputIndex + 5;
+        let nextValue = aToZ[nextIndex];
+    }
+}
+
+
+
+
+
+//decode(str)
