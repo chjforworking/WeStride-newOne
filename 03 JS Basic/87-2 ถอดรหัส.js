9ad8df4eb82d3154439361816061c562742e5e02
chjforwork <chjforworking@gmail.com>
2024-12-06 12:48:34 +0700

    06/12

--- 03 JS Basic/87-2 ถอดรหัส.js
+++ 03 JS Basic/87-2 ถอดรหัส.js
@@ -1,23 +1,17 @@
-//สร้างโปรแกรมถอดรหัส 5 ลำดับ
-/*
-1.สร้าง array a-z
-2.สร้าง function
-3.แยกตัวอักษร
-4.หา index ของ input ที่รับเข้ามา
-*/
-
-let arr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+let arr = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
+    "n","o","p","q","r","s","t","u","v","w","x","y","z",];
 let newArr = [];
-function decrypt(str){ //str =xvo + 5 ก่อนจะบวกห้าต้องแยกแต่ละตัวก่อน
+
+function decrypt (str){
     let splitStr = str.split("");
-    for (const element of splitStr) {
-        let inputIndex = arr.indexOf(element);
-        let secIndex = inputIndex + 5 ;
-        let value = arr[secIndex];
-        if(value){
-            newArr.push(value)
-        }
+    for (const x of splitStr) {
+        let inputIndex = arr.indexOf(x);
+        let secIndex = (inputIndex+5) % 26;
+        let value = arr[secIndex]
+        newArr.push(value)
     }
+    console.log(newArr.join(""));
+    
 }
 
-console.log(decrypt("xvt"));
+decrypt("xvo")
--- 03 JS Basic/88 เข้ารหัส.js
+++ 03 JS Basic/88 เข้ารหัส.js
@@ -1,22 +1,19 @@
 //function เข้ารหัส A -> N , B -> O , C -> P
+//14 ตำแหน่ง
 
-let aToZ = ["a","b","c",'d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let aToZ = ["a","b","c",'d','e','f','g','h','i','j','k','l','m','n',
+    'o','p','q','r','s','t','u','v','w','x','y','z'];
+let ansArr = [];
 
-function decode(str){
-    let splitStr = str.split('');
+function decrypt(str){
+    let splitStr = str.split("");
     for (const x of splitStr) {
-        let inputIndex = aToZ.indexOf(x) ;//1,2,3
-        let nextIndex = inputIndex + 5;
-        let ansValue = aToZ[nextIndex];
-        if(){
-            
-        }
+        let inputIndex = aToZ.indexOf(x); //1 2 3 4 5
+        let secIndex = (inputIndex - 13 + 26) % 26;
+        let value = aToZ[secIndex];
+        ansArr.push(value)
     }
+    console.log(ansArr.join(","));
 }
 
-
-
-
-console.log(decode("xvo"));
-
-//decode(str)
+decrypt("uryyb")
