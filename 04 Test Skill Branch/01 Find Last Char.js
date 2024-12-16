e32f2edbd53cf7c63f8e378bbe0e88887f49f5d1
chjforwork <chjforworking@gmail.com>
2024-12-16 13:07:00 +0700

    16/12 2

---
+++ 04 Test Skill Branch/01 Find Last Char.js
@@ -0,0 +1,8 @@
+function findLastChar(str) {
+  let len = str.length;
+  let lastChar = len - 1;
+  let result = str[lastChar];
+  console.log(result);
+}
+
+findLastChar("Hello");
---
+++ 04 Test Skill Branch/02 String to array.js
@@ -0,0 +1,8 @@
+//แปลง string to array;
+
+function convertToArr(str){
+    const result = str.split("");
+    console.log(result);
+}
+
+convertToArr("Hello")
