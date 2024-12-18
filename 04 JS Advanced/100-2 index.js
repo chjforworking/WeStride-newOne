0d36c750a73ca1842a1d12096aa2270edba422a4
chjforwork <chjforworking@gmail.com>
2024-12-18 14:46:53 +0700

    18/12 (1)

--- 04 JS Advanced/100-2 index.js
+++ 04 JS Advanced/100-2 index.js
@@ -1,12 +1,11 @@
 let number = document.getElementById("number");
 
-
 let count = 0;
 setInterval(() => {
   if (count == 65) {
     clearInterval;
   } else {
     count += 1;
-    number.innerHTML = count + '%';
+    number.innerHTML = count + "%";
   }
-},28);
+}, 28);
