425dab8139226cc611d7383a8b22cc59579b1bbe
PC <chjforworking@gmail.com>
2025-01-08 14:09:51 +0700

    8/1 - 2

--- Live Test/JS Basic/09-1.js
+++ Live Test/JS Basic/09-1.js
@@ -7,32 +7,90 @@ output: เหรียญหรือแบงค์ที่ต้องทอ
 Output: ทอนด้วยแบงค์ 50 1 ใบ, เหรียญ 10 1 เหรียญ, เหรียญ 5 1 เหรียญ
 */
 
-function changeCalcu(price, paid) {
-  let change = paid - price;
-  let moneyType = [1000, 500, 100, 50, 20, 10, 5, 2, 1]
-  let moneyByType = {};
-
-  for(let x of moneyType){
-    let unit = Math.floor(change/x);
-    if(unit>0){
-        
+function calcu(priceTag, money) {
+  let changeMoney = money - priceTag;
+  console.log(`ต้องทอนเงิน ${changeMoney}`);
+  //ทอน 1000
+  if (changeMoney > 1000) {
+    let count1 = 0;
+    for (let i = 0; changeMoney >= 1000; i++) {
+      changeMoney -= 1000;
+      count1++;
     }
+    console.log(`ทอนธนบัตร 1000 : ${count1} ใบ`);
+  }
+  //ทอน 500
+  if (changeMoney >= 500 && changeMoney < 1000) {
+    let count2 = 0;
+    for (let i = 0; changeMoney >= 500; i++) {
+      changeMoney -= 500;
+      count2++;
+    }
+    console.log(`ทอนธนบัตร 500 : ${count2} ใบ`);
+  }
+  //ทอน 100
+  if (changeMoney >= 100 && changeMoney < 500) {
+    let count3 = 0;
+    for (let i = 0; changeMoney >= 100; i++) {
+      changeMoney -= 100;
+      count3++;
+    }
+    console.log(`ทอนธนบัตร 100 : ${count3} ใบ`);
+  }
+  //ทอน 50
+  if (changeMoney >= 50 && changeMoney < 100) {
+    let count4 = 0;
+    for (let i = 0; changeMoney >= 50; i++) {
+      changeMoney -= 50;
+      count4++;
+    }
+    console.log(`ทอนธนบัตร 50 : ${count4} ใบ`);
+  }
+  //ทอน 20
+  if (changeMoney >= 20 && changeMoney < 50) {
+    let count5 = 0;
+    for (let i = 0; changeMoney >= 20; i++) {
+      changeMoney -= 20;
+      count5++;
+    }
+    console.log(`ทอนธนบัตร 20 : ${count5} ใบ`);
+  }
+  //ทอน 10
+  if (changeMoney >= 10 && changeMoney < 20) {
+    let count6 = 0;
+    for (let i = 0; changeMoney >= 10; i++) {
+      changeMoney -= 10;
+      count6++;
+    }
+    console.log(`ทอนเหรียญ 10 : ${count6} เหรียญ`);
+  }
+  //ทอน 5
+  if (changeMoney >= 5 && changeMoney < 10) {
+    let count7 = 0;
+    for (let i = 0; changeMoney >= 5; i++) {
+      changeMoney -= 5;
+      count7++;
+    }
+    console.log(`ทอนเหรียญ 5 : ${count7} เหรียญ`);
+  }
+  //ทอน 2
+  if (changeMoney >= 2 && changeMoney < 5) {
+    let count8 = 0;
+    for (let i = 0; changeMoney >= 2; i++) {
+      changeMoney -= 2;
+      count8++;
+    }
+    console.log(`ทอนเหรียญ 2 : ${count8} เหรียญ`);
+  }
+  //ทอน 1
+  if (changeMoney >= 1 && changeMoney < 2) {
+    let count9 = 0;
+    for (let i = 0; changeMoney >= 1; i++) {
+      changeMoney -= 1;
+      count9++;
+    }
+    console.log(`ทอนเหรียญ 1 : ${count9} เหรียญ`);
   }
 }
-/*
-function calculateChange(ราคาสินค้า, เงินที่จ่าย) {
-  let เงินทอน = เงินที่จ่าย - ราคาสินค้า;
-  const ธนบัตรเหรียญ = [1000, 500, 100, 50, 20, 10, 5, 2, 1];
-  const จำนวนเงินทอนแต่ละชนิด = {};
 
-  for (let ธนบัตร of ธนบัตรเหรียญ) {
-    const จำนวนเหรียญ = Math.floor(เงินทอน / ธนบัตร);
-    if (จำนวนเหรียญ > 0) {
-      จำนวนเงินทอนแต่ละชนิด[ธนบัตร] = จำนวนเหรียญ;
-      เงินทอน -= ธนบัตร * จำนวนเหรียญ;
-    }
-  }
-
-  return จำนวนเงินทอนแต่ละชนิด;
-}
-*/
+calcu(35, 100);
