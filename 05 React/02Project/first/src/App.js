f85f9877cd2653045df750f2ba4841a418471109
chjforwork <chjforworking@gmail.com>
2025-01-16 16:39:13 +0700

    16/2 updated

--- 05 React/02Project/first/src/App.js
+++ 05 React/02Project/first/src/App.js
@@ -1,12 +1,15 @@
 import ProfileCard from "./ProfileCard";
+import alexaImg from "./Images/alexa.png";
+import cortanaImg from "./Images/cortana.png";
+import siriImg from "./Images/siri.png";
 
 function App() {
   return (
     <div>
       <h1>Personal Digital Assistansts</h1>
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
+      <ProfileCard title={"Alexa"} handle={"@alexa99"} img={alexaImg} />
+      <ProfileCard title={"Cortana"} handle={"@cortana26"} img={cortanaImg} />
+      <ProfileCard title={"Siri"} handle={"@siri01"} img={siriImg} />
     </div>
   );
 }
--- 05 React/02Project/first/src/ProfileCard.js
+++ 05 React/02Project/first/src/ProfileCard.js
@@ -1,5 +1,12 @@
-function ProfileCard() {
-  return <h2>ProfileCard</h2>;
+function ProfileCard(props) {
+  const { title, handle, img } = props;
+  return (
+    <div>
+      <img src={img} alt="ProfileCardImage" />
+      <h2>Title is {title}</h2>
+      <h4>Handle is {handle}</h4>
+    </div>
+  );
 }
 
 export default ProfileCard;
