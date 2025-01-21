91595baac914c039f0a59d8aedf617849954faea
chjforwork <chjforworking@gmail.com>
2025-01-21 08:18:43 +0700

    21/1

--- 05 React/02Project/2animals/src/App.js
+++ 05 React/02Project/2animals/src/App.js
@@ -1,11 +1,25 @@
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog","gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)]; //animals[0] animals ตำแหน่งที่แบบสุ่ม
+}
+
 function App() {
-    const handleClick = ()=>{
-        console.log("Hello");
-        
-    }
+  const [animals, setAnimal] = useState([]);
+  const handleClick = () => {
+    // [] click -> cat click -> bird
+    setAnimal([...animals, getRandomAnimal()]);
+  };
+  const renderAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
   return (
     <div>
-      <button onClick={handleClick}>Click to add more animal</button>
+      <button onClick={handleClick}>Add Animal</button>
+      <div>{renderAnimals}</div>
     </div>
   );
 }
