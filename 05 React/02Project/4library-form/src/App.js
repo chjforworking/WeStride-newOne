f8a77bb4c7832c9be4cbd46286b57577b388c409
PC <chjforworking@gmail.com>
2025-02-06 16:56:57 +0700

    6/2

--- 05 React/02Project/4library-form/src/App.js
+++ 05 React/02Project/4library-form/src/App.js
@@ -1,7 +1,30 @@
-import "./App.css";
+import { useState } from "react";
+import BookCreate from "./component/BookCreate";
+import BookList from "./component/BookList";
 
 function App() {
-  return <div>Hello</div>;
+  const [books, setBooks] = useState([]);
+
+  const deleteBookById = (id) => {
+    const updatedBook = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBook);
+  };
+
+  const createBook = (title) => {
+    const updatedBook = [
+      ...books,
+      { id: Math.round(Math.random() * 9999), title },
+    ];
+    setBooks(updatedBook);
+  };
+  return (
+    <div className="App">
+      <BookList books={books} onDelete = {deleteBookById}/>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
