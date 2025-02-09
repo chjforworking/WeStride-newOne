e6e5e3c9aa420f9b74abed5638d797a9c649e29f
PC <chjforworking@gmail.com>
2025-02-09 19:58:55 +0700

    9/2 1

--- 05 React/02Project/4library-form/src/App.js
+++ 05 React/02Project/4library-form/src/App.js
@@ -1,37 +1,55 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./component/BookCreate";
 import BookList from "./component/BookList";
+import axios from "axios";
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const editBookById = (id, newTitle) => {
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    setBooks(response.data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
+    console.log(response);
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
-      } else {
-        return book;
+        return { ...book, ...response.data };
       }
+      return book;
     });
     setBooks(updatedBooks);
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete("http://localhost:3001/books/" + id);
+
     const updatedBook = books.filter((book) => {
       return book.id !== id;
     });
     setBooks(updatedBook);
   };
 
-  const createBook = (title) => {
-    const updatedBook = [
-      ...books,
-      { id: Math.round(Math.random() * 9999), title },
-    ];
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title: title,
+    });
+
+    const updatedBook = [...books, response.data];
     setBooks(updatedBook);
   };
   return (
-    <div className="App">
+    <div className="app">
       <h1>Reading List</h1>
       <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
