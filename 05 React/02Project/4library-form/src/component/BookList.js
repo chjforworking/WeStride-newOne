53187f5229a6ff83a4e945961b265f8b3e1043e9
PC <chjforworking@gmail.com>
2025-02-10 09:33:16 +0700

    19/2

--- 05 React/02Project/4library-form/src/component/BookList.js
+++ 05 React/02Project/4library-form/src/component/BookList.js
@@ -1,6 +1,9 @@
 import BookShow from "./BookShow";
+import { useContext } from "react";
+import BooksContext from "../context/book";
 
 function BookList({ books, onDelete, onEdit }) {
+  const value = useContext(BooksContext)
   const renderedBooks = books.map((book) => {
     return (
       <BookShow onEdit={onEdit} key={book.id} book={book} onDelete={onDelete} />
