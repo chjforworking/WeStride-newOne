71d69bb18912b40177f47577806daca716915149
PC <chjforworking@gmail.com>
2025-02-19 21:19:32 +0700

    19/2 2

--- 05 React/02Project/5location-search/src/api/Place.ts
+++ 05 React/02Project/5location-search/src/api/Place.ts
@@ -1,8 +1,8 @@
 export interface Place {
-    id : number;
-    name: string;
-    longitude : number;
-    latitude : number;
+  id: number;
+  name: string;
+  longitude: number;
+  latitude: number;
 }
 
 //import type {Place} from '/...'
