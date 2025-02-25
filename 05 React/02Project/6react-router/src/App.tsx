e4137a97eb898d428b96fbb5fa99d03d24f598e7
PC <chjforworking@gmail.com>
2025-02-25 14:26:32 +0700

    25/2 (1)

--- 05 React/02Project/6react-router/src/App.tsx
+++ 05 React/02Project/6react-router/src/App.tsx
@@ -1,7 +1,32 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./pages/RootLayout";
+import HomePage from "./pages/HomePage";
+import DetailsPage from "./pages/DetailsPage";
+import SearchPage from "./pages/SearchPage";
+
+const $router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/search",
+        element: <SearchPage />,
+      },
+      {
+        path: "/packages/:name",
+        element: <DetailsPage />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <div>Hello</div>
-  )
+  return <RouterProvider router={$router}/>
 }
 
-export default App
+export default App;
---
+++ 05 React/02Project/6react-router/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom";
+import SearchInput from "./SearchInput";
+
+export default function Header() {
+  return (
+    <div className="flex items-center justify-between px-4 border-b h-14">
+      <div className="flex items-center space-x-2 text-sm">
+        <NavLink className="text-lg font-bold" to="/">
+          NPM Registry
+        </NavLink>
+      </div>
+      <div className="w-full max-w-xl ml-4">
+        <SearchInput />
+      </div>
+    </div>
+  );
+}
